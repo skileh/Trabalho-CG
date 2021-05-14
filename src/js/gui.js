514077bc34e74c3b896a71fa828a120968b7b0c6
skileh <heliksmm@hotmail.com>
2021-05-14 09:22:16 -0300

    Camera translation

--- src/js/gui.js
+++ src/js/gui.js
@@ -1,32 +1,41 @@
 var config = { y_rotate: degToRad(0),  x_rotate: degToRad(0), z_rotate: degToRad(0),
 				y_translation: degToRad(0),  x_translation: degToRad(0), z_translation: degToRad(0),
-      beizer_translation: degToRad(0)};
+        beizer_translation: degToRad(0), 
+        y_scale: degToRad(2),  x_scale: degToRad(2), y_scale: degToRad(2),
+        cam1PosX: degToRad(2),  cam1PosY: degToRad(2), cam1PosZ: degToRad(2)};
 
-var button1 = { Add_new_object:function(){ val = 0, count +=1, isCreate=true}};
+var button1 = { Add_new_object:function(){ isCreate=true}};
 var button2 = { remove_obj:function(){ isRemove=true}};
-var select = { Select_Object:function(){ if(isSelect == '0'){
-  val = 0;
-} 
-}};
 
-var num;
 const loadGUI = () => {
 
 
   const gui = new dat.GUI();
   gui.add(button1, 'Add_new_object');
   gui.add(button2, 'remove_obj');
-  gui.add(select, 'Select_Object', num); 
 
-  const rotate = gui.addFolder('Rotação no eixo');
+  const rotate = gui.addFolder('Rotação no Eixo Objeto');
   rotate.add(config, "y_rotate", 0, 20, 0.5);
   rotate.add(config, "x_rotate", 0, 20, 0.5);
   rotate.add(config, "z_rotate", 0, 20, 0.5);
 
-  const translate = gui.addFolder('Translação');
+  const translate = gui.addFolder('Translação Objeto');
   translate.add(config, "y_translation", -100, 100, 0.5);
   translate.add(config, "x_translation", -100, 100, 0.5);
   translate.add(config, "z_translation", -100, 100, 0.5);
   translate.add(config, "beizer_translation", 0,1,0.001);
+
+  const scale = gui.addFolder('Escala Objeto');
+  scale.add(config, "y_scale", 0, 10, 0.01);
+  scale.add(config, "x_scale", 0, 10, 0.01);
+  scale.add(config, "y_scale", 0, 10, 0.01);
+
+  const cam_position = gui.addFolder('Translação Camêra');
+  cam_position.add(config, "cam1PosX", -200, 200, 1);
+  cam_position.add(config, "cam1PosY", -200, 200, 1);
+  cam_position.add(config, "cam1PosZ", -200, 200, 1);
+
+
+
 
 };
--- src/js/script.js
+++ src/js/script.js
@@ -34,9 +34,20 @@ void main() {
 }
 `;
 
-var val=0;
-var count=0;
-var index=0;
+function bezier(t, p1, p2, p3, p4) {
+  var invT = (1 - t)
+  var px = ((p1[0]) * invT * invT * invT) +
+    ((p2[0]) * 3 * t * invT * invT) +
+    (p3[0] * 3 * invT * t * t) +
+    (p4[0] * t * t * t);
+  var py = ((p1[1]) * invT * invT * invT) +
+    (p2[1] * 3 * t * invT * invT) +
+    (p3[1] * 3 * invT * t * t) +
+    ((p4[1]) * t * t * t);
+
+  return [px, py];
+}
+
 var isCreate = false;
 var isRemove = false;
 function main() {
@@ -52,9 +63,9 @@ function main() {
   // normal with a_normal etc..
   twgl.setAttributePrefix("a_");
   // setup GLSL program,
-  var coneBufferInfos   = flattenedPrimitives.createTruncatedConeBufferInfo(gl, 10, 0, 20, 12, 1, true, false);
+  var coneBufferInfos = flattenedPrimitives.createTruncatedConeBufferInfo(gl, 10, 0, 20, 12, 1, true, false);
   var programInfo = twgl.createProgramInfo(gl, [vs, fs]);
-  var coneVAOs   = twgl.createVAOFromBufferInfo(gl, programInfo, coneBufferInfos);
+  var coneVAOs = twgl.createVAOFromBufferInfo(gl, programInfo, coneBufferInfos);
 
   function degToRad(d) {
     return d * Math.PI / 180;
@@ -64,25 +75,43 @@ function main() {
 
   // Uniforms for each object.
   var coneUniforms = [];
-  var coneTranslation =[];
-  var coneBezier =[];
-  var coneYRotation=[];
-  var coneXRotation=[];
-  var coneZRotation=[];
 
+  //armazena posições da translação bezier
+  var positions = [];
+  
+  //Atributos dos objetos
+  var coneTranslation = [];
+  var coneBezier = []; //armazena a translação bezier do cone
+  var coneYRotation = [];
+  var coneXRotation = [];
+  var coneZRotation = [];
+  var coneXScale = [];
+  var coneYScale = [];
+  var coneZScale = [];
   var objectsToDraw = [];
 
-  function computeMatrix(viewProjectionMatrix, translation, xRotation, yRotation, zRotation) {
+  //Atributo das cameras
+  const cameraPosition = [
+    config.cam1PosX=0,
+    config.cam1PosY=0,
+    config.cam1PosZ=100,
+  ];
+  var target = [0, 0, 0];
+  var up = [0, 1, 0];
+
+  //
+  function computeMatrix(viewProjectionMatrix, translation, xRotation, yRotation, zRotation, xScale, yScale, zSacale) {
     var matrix = m4.translate(viewProjectionMatrix,
-        translation[0],
-        translation[1],
-        translation[2]);
+      translation[0],
+      translation[1],
+      translation[2]);
+    matrix = m4.scale(matrix, xScale, yScale, zSacale);
     matrix = m4.xRotate(matrix, xRotation);
-    matrix = m4.zRotate(matrix,zRotation);
+    matrix = m4.zRotate(matrix, zRotation);
     return m4.yRotate(matrix, yRotation);
   }
 
-  loadGUI(objectsToDraw);
+  loadGUI();
   requestAnimationFrame(drawScene);
   // Draw the scene.
   function drawScene(time) {
@@ -99,124 +128,184 @@ function main() {
     // Compute the projection matrix
     var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
     var projectionMatrix =
-        m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
+      m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
 
     // Compute the camera's matrix using look at.
-    var cameraPosition = [0, 0, 100];
-    var target = [0, 0, 0];
-    var up = [0, 1, 0];
+    const cameraPosition = [
+      config.cam1PosX,
+      config.cam1PosY,
+      config.cam1PosZ,
+    ];
     var cameraMatrix = m4.lookAt(cameraPosition, target, up);
 
     // Make a view matrix from the camera matrix.
     var viewMatrix = m4.inverse(cameraMatrix);
 
     var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
-    
-    
-   
-      
-      if(coneTranslation[count-1] !=[
-                        config.x_translation,
-                        config.y_translation,
-                        config.z_translation
-                        ]   ){
-        coneTranslation[count-1] = [
-                        config.x_translation,
-                        config.y_translation,
-                        config.z_translation
-                        ];
-                      
 
-      }
 
-      if(coneBezier[count-1]!=config.beizer_translation){
-        var t = config.beizer_translation;
-        var invT = (1 - t)
-        var Px = ((-60) * invT *invT *invT) +
-            ((-60) * 3 * t * invT *invT )+
-            (50 * 3 * invT * t * t)+
-            (50 * t * t * t);
-        var Py= ((-30) * invT *invT *invT )+
-            (30 * 3 * t * invT *invT )+
-           ( 30 * 3 * invT * t * t )+
-            ((-30) * t * t * t);
-        coneTranslation[count-1] = [
-                        Px,
-                        Py,
-                        config.z_translation
-                        ];
-        config.x_translation=Px;
-        config.y_translation=Py;
-        coneBezier[count-1] = config.beizer_translation;
-      }
-      coneXRotation[count-1] =  config.x_rotate;
-      coneYRotation[count-1] =  config.y_rotate;
-      coneZRotation[count-1] =  config.z_rotate;
 
-    if(isCreate){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // verifica se o slice de translação foi modificado
+    if (coneTranslation[objectsToDraw.length - 1] != [
+      config.x_translation,
+      config.y_translation,
+      config.z_translation
+    ]) {
+      coneTranslation[objectsToDraw.length - 1] = [
+        config.x_translation,
+        config.y_translation,
+        config.z_translation
+      ];
+
+
+    }
+
+    //verifica se o slice de translação bezier foi ativo
+    if (coneBezier[objectsToDraw.length - 1] != config.beizer_translation) {
+      positions = bezier(config.beizer_translation,
+        [-60, -30],
+        [-60, 30],
+        [50, 30],
+        [50, -30]);
+
+      coneTranslation[objectsToDraw.length - 1] = [
+        positions[0],
+        positions[1],
+        config.z_translation
+      ];
+      config.x_translation = positions[0];
+      config.y_translation = positions[1];
+      coneBezier[objectsToDraw.length - 1] = config.beizer_translation;
+    }
+    coneXRotation[objectsToDraw.length - 1] = config.x_rotate;
+    coneYRotation[objectsToDraw.length - 1] = config.y_rotate;
+    coneZRotation[objectsToDraw.length - 1] = config.z_rotate;
+
+    coneXScale[objectsToDraw.length - 1] = config.x_scale;
+    coneYScale[objectsToDraw.length - 1] = config.y_scale;
+    coneZScale[objectsToDraw.length - 1] = config.z_scale;
+
+    //verifica se um objeto deve ser criado
+    if (isCreate) {
 
       var coneUniformss =
-        {
-          u_colorMult: [time, -time, 1, 1],
-          u_matrix: m4.identity(),
-        };
-      coneTranslation.push([ 0, 0, 0]);
+      {
+        u_colorMult: [time, -time, 1, 1],
+        u_matrix: m4.identity(),
+      };
+      coneTranslation.push([0, 0, 0]);
       coneYRotation.push(0);
       coneXRotation.push(0);
       coneZRotation.push(0);
+      coneXScale.push(1);
+      coneZScale.push(1);
+      coneYScale.push(1);
       coneBezier.push(0);
-     
+      config.x_scale = 0.5;
+      config.z_scale = 0.5;
+      config.y_scale = 0.5;
+
 
       // setup GLSL program
 
       objectsToDraw.push({
-          bufferInfo: coneBufferInfos,
-          uniforms: coneUniformss,
+        bufferInfo: coneBufferInfos,
+        uniforms: coneUniformss,
       });
       isCreate = false;
       console.log(objectsToDraw);
     }
 
-    if(isRemove){
+    if (isRemove) {
       //sempre deixa apenas 1 objeto na tela
-      if(count == 1){
-        isRemove=false;
+      if (objectsToDraw.length == 1) {
+        isRemove = false;
       }
-      else{
+      else {
         //remove o ultimo objeto inserido
-        objectsToDraw.splice(0,1);
-        coneTranslation.splice(0,1);
-        coneYRotation.splice(0,1);
-        coneXRotation.splice(0,1);
-        coneZRotation.splice(0,1);
-        coneBezier.splice(0,1);
+        objectsToDraw.splice(0, 1);
+        coneTranslation.splice(0, 1);
+        coneYRotation.splice(0, 1);
+        coneXRotation.splice(0, 1);
+        coneZRotation.splice(0, 1);
+        config.x_translation = 0;
+        config.y_translation = 0;
+        config.z_translation = 0;
+        config.x_rotate = 0;
+        config.y_rotate = 0;
+        config.z_rotate = 0;
+        config.beizer_translation = 0;
+        coneBezier.splice(0, 1);
         console.log(objectsToDraw.length)
-        isRemove=false;
-        count -=1;
+        isRemove = false;
       }
     }
 
     // Compute the matrices for each object.
-    var c=0;
-    objectsToDraw.forEach(function(object) {
+    var c = 0;
+    objectsToDraw.forEach(function (object) {
       object.uniforms.u_matrix = computeMatrix(
-          viewProjectionMatrix,
-          coneTranslation[c],
-          coneXRotation[c],
-          coneYRotation[c],
-          coneZRotation[c]);
-      
-      c +=1;
+        viewProjectionMatrix,
+        coneTranslation[c],
+        coneXRotation[c],
+        coneYRotation[c],
+        coneZRotation[c],
+        coneXScale[c],
+        coneYScale[c],
+        coneZScale[c]);
+
+      c += 1;
     });
 
-      // Setup all the needed attributes.
+    // Setup all the needed attributes.
     // desenha 1 cone \  malha que vai ser utilizada
     gl.bindVertexArray(coneVAOs);
     // programa que será utilizado
     gl.useProgram(programInfo.program);
 
     // ------ Draw the objects --------
-    objectsToDraw.forEach(function(object) {
+    objectsToDraw.forEach(function (object) {
       // Set the uniforms we just computed
       twgl.setUniforms(programInfo, object.uniforms);
       twgl.drawBufferInfo(gl, object.bufferInfo);
